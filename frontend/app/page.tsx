65801431a1c2229387ee30476db949d5bd28d9e7
SOLSURPRISE <alineyef4@gmail.com>
2025-12-29 00:09:41 +0200

    Add files via upload

---
+++ frontend/app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (<div className="min-h-screen min-w-full flex justify-center items-center text-3xl bg-neutral-800 text-white">
+    Comming Soon!
+  </div>);
+}
